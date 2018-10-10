9a0aba8c91b0795e01d90122f08c8621ce9431f2
moraw <krzymoraw@gmail.com>
2018-10-10 16:04:55 +0200

    Removed old python files which were using GPU functions

--- Main.py
+++
@@ -1,8 +0,0 @@
-from numba import cuda
-import GUI2 as gui
-
-cuda.select_device(0)
-dev = cuda.get_current_device()
-print('CUDA device in use: ' + dev.name.decode())
-
-gui.RunTriangulationWindow()
